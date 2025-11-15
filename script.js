d82f1a9ed927c10c3f03cf172fdcf64cc4955fc6
architneupane <architneupane55@gmail.com>
2025-11-15 15:01:31 +0545

    Second Commit

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     index = (index + 2) % phrases.length;
   }
 
-  setInterval(updateText, 3000); // Change text every 3 seconds
-  updateText(); // Initialize with the first phrase
+  setInterval(updateText, 3000);
+  updateText();
 });
